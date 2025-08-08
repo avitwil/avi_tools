e009ca14de0e597ddac3700855aa5b5e48a0e208
avitwil <avitwil@gmail.com>
2025-08-08 17:54:54 +0300

    Update __init__.py

--- avi_tools/__init__.py
+++ avi_tools/__init__.py
@@ -1,3 +1,4 @@
 from .async_funcs_manager import CapsFunc,FuncsToAsync,AsyncRunner
-from .twillkit import Monoid, Colors, Infix, InteractivePathSelectFileCreation ,CreateFolderForFileCreation
+from .twillkit import Monoid, Colors, Infix, InteractivePathSelectFileCreation ,CreateFolderForFileCreation ,catch_exceptions
+
 from .variabledb import VariableDB
